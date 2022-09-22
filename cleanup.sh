5f742b9219cf2046fa540684c2f7c491d60342ee
Mohammed Shamim <shamimice03@gmail.com>
2022-09-22 14:21:44 +0900

    Update and rename cleanup-script.sh to cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -6,15 +6,21 @@ read -p 'Enter the Cluster Name : ' clustername
 
 export CLUSTER_NAME=$clustername
 export POLICY_ARN=$(aws iam list-policies --query 'Policies[?PolicyName==`AmazonEKSClusterAutoscalerPolicy`].Arn' --output text)
+export ROLE_NAME='AmazonEKSClusterAutoscalerRole'
 
-# Delete cluster-autoscaler
-kubectl delete -f cluster-autoscaler.yaml
+echo "Deleting IAM Role and Policies"
+# Delete IAM Role and Policies
+aws iam detach-role-policy --role-name=${ROLE_NAME}  --policy-arn=${POLICY_ARN}
+aws iam delete-role --role-name=${ROLE_NAME}
+aws iam delete-policy --policy-arn=${POLICY_ARN}
+
+
 
 # Delete IAM Role and Service Account　
 eksctl delete iamserviceaccount  \
  --cluster=${CLUSTER_NAME}  \
  --name=cluster-autoscaler \
  --namespace=kube-system
-
-# Delete policy
-aws iam delete-policy --policy-arn ${POLICY_ARN}
+ 
+# Delete cluster-autoscaler
+kubectl delete -f cluster-autoscaler.yaml
